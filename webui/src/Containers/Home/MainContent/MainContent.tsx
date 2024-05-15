a5b567d7f2f5bb5e5bf4272cb59f1d17a3055f84
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-05-15 11:20:46 +0000

    fix styles

--- webui/src/Containers/Home/MainContent/MainContent.tsx
+++ webui/src/Containers/Home/MainContent/MainContent.tsx
@@ -16,8 +16,17 @@ const MainContent: FC<MainContentProps> = (props) => {
         <RunButton>New Project</RunButton>
       </Header>
       {projects!.map((item, index) => (
-        <Link to={`/project/${item.id}`} key={index}>
-          <ProjectWrapper>{item.name}</ProjectWrapper>
+        <Link
+          to={`/project/${item.id}`}
+          key={index}
+          style={{ marginBottom: 25 }}
+        >
+          <ProjectWrapper>
+            <div>
+              <div>{item.name}</div>
+              <div style={{ fontSize: 13, color: "#14ffb5" }}>{item.path}</div>
+            </div>
+          </ProjectWrapper>
         </Link>
       ))}
     </Container>
