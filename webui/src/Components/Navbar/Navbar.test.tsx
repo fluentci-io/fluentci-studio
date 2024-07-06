16c3b29dce16cb70dc251bc6af61291cbaf31388
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-06 07:34:53 +0000

    chore: update test snapshots

--- webui/src/Components/Navbar/Navbar.test.tsx
+++ webui/src/Components/Navbar/Navbar.test.tsx
@@ -1,11 +1,24 @@
 import { render } from "@testing-library/react";
 import Navbar from "./Navbar";
 import { BrowserRouter } from "react-router-dom";
+import { FC, ReactNode } from "react";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 describe("Navbar", () => {
   it("should render the component", () => {
+    const wrapper: FC<{ children: ReactNode }> = ({ children }) => (
+      <ClerkProvider
+        routerPush={vi.fn()}
+        routerReplace={vi.fn()}
+        publishableKey={
+          "pk_test_c3BsZW5kaWQtcmFiYml0LTMyLmNsZXJrLmFjY291bnRzLmRldiQ"
+        }
+      >
+        <BrowserRouter>{children}</BrowserRouter>
+      </ClerkProvider>
+    );
     const { container } = render(<Navbar onSignOut={vi.fn()} />, {
-      wrapper: BrowserRouter,
+      wrapper,
     });
     expect(container).toMatchSnapshot();
   });
