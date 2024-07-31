808031d62f0ab7435798de3673ca697fc6961236
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-31 10:09:40 +0000

    fix empty organizations
    fix empty organizations

--- webui/src/Containers/LinkProject/MainContent/MainContent.tsx
+++ webui/src/Containers/LinkProject/MainContent/MainContent.tsx
@@ -1,15 +1,34 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Container } from "./styles";
 import Header from "../../../Components/Header";
 import Organizations from "./Organizations";
 import Repositories from "./Repositories";
+import { useAuth } from "@clerk/clerk-react";
 
 const MainContent: FC = () => {
+  const { getToken } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const token = await getToken({
+        skipCache: true,
+      });
+      localStorage.setItem("token", token!);
+      setLoading(false);
+    };
+    fetchToken();
+  }, [getToken]);
+
   return (
     <Container>
       <Header />
-      <Organizations />
-      <Repositories />
+      {!loading && (
+        <>
+          <Organizations />
+          <Repositories />
+        </>
+      )}
     </Container>
   );
 };
